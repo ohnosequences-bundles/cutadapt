eca1bf89617d19a7a26c652cd3f9c42dab0dd471
Marina Manrique <mmanrique@era7.com>
2015-07-17 16:10:33 +0200

    add cutadapt bundle

--- src/main/scala/cutadapt.scala
+++ src/main/scala/cutadapt.scala
@@ -3,10 +3,10 @@ package ohnosequencesBundles.statika
 import ohnosequences.statika._, bundles._, instructions._
 
 
-case object cutadapt extends Bundle() {
+case object cutadapt extends Bundle(cdevel) {
 
   def install: Results = {
-    // do someting here
+    Seq("pip", "install", "cutadapt") ->-
     success(s"${bundleName} is installed")
   }
 
