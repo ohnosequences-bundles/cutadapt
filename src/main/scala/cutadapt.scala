a65df58114820bfd46e9ff0169f91ae0880e91ac
Marina Manrique <mmanrique@era7.com>
2015-11-24 12:17:03 +0100

    change Cutadapt objet to abstract class

--- src/main/scala/cutadapt.scala
+++ src/main/scala/cutadapt.scala
@@ -3,7 +3,7 @@ package ohnosequencesBundles.statika
 import ohnosequences.statika._, bundles._, instructions._
 
 
-case object Cutadapt extends Bundle(cdevel) { cutadapt =>
+abstract class Cutadapt extends Bundle(cdevel) { cutadapt =>
 
   lazy val install = cmd("pip")("install", "cutadapt")
 
