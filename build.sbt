c6f6c5a925d46cfcec71c159d9e83855be0d6863
Marina Manrique <mmanrique@era7.com>
2015-12-03 11:33:25 +0100

    fix cdevel dep and install method

--- build.sbt
+++ build.sbt
@@ -13,5 +13,5 @@ resolvers ++= Seq(
 
 libraryDependencies ++= Seq(
  "ohnosequences"           %% "statika"    % "2.0.0-M4",
- "ohnosequences-bundles"   %% "cdevel"     % "0.4.0-SNAPSHOT"
+ "ohnosequences-bundles"   %% "cdevel"     % "0.3.0"
 )
--- src/main/scala/cutadapt.scala
+++ src/main/scala/cutadapt.scala
@@ -5,7 +5,7 @@ import ohnosequences.statika._, bundles._, instructions._
 
 abstract class Cutadapt extends Bundle(cdevel) { cutadapt =>
 
-  lazy val install = cmd("pip")("install", "cutadapt")
+  lazy val install = cmd("pip")("install", "--install-option=--install-scripts=/usr/bin", "cutadapt")
 
   def instructions: AnyInstructions = install
 
